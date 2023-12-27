bce17be9475a68564d02e6d0b920f24538889421
Yan Minagawa <yan@minagawa.de>
2023-12-27 22:15:45 +0100

    some version with verification

--- beerbot.py
+++ beerbot.py
@@ -4,15 +4,29 @@ from maubot.handlers import command
 # import RPi.GPIO as GPIO
 import subprocess
 import time
+import requests
 
 class BeerBot(Plugin):
   @command.new()
-  async def beer(self, evt: MessageEvent) -> None:
+  async def stalebeer(self, evt: MessageEvent) -> None:
     await evt.reply("more beer please!!")
     gpio21 = "/sys/class/gpio/gpio21/value"
     subprocess.run(["/bin/sh", "-c", "echo 1 > " + gpio21])
     time.sleep(18)
     subprocess.run(["/bin/sh", "-c", "echo 0 > " + gpio21])
-  
+
 
     # os.system("/home/candle/beertap/pour.sh")
+  @command.new()
+  async def beer(self, evt: MessageEvent) -> None:
+    url = "http://127.0.0.1:5000/setEmoji"
+    data = {"emoji":"🍺"}
+    r = requests.post (url, json = data)
+    response = r.json()
+    if response.get("success"):
+      await evt.reply("find the 🍺 on the screen")
+    else: 
+      await evt.reply("bar is closed or the barbot is drunk")
+
+    # find the username and safe the use name as order person for 1 minute
+    # after 1 minute empty the order 
